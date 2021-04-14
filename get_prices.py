c75d73de8833d954f7da9a143f969f1d5547779f
Diwash Rai <diwash.rai1993@gmail.com>
2021-04-14 23:45:20 +0100

    first commit for repo to contain python scripts that i will use to keep my crypto tracking google sheets updated

---
+++ get_prices.py
@@ -0,0 +1,12 @@
+import ccxt
+from datetime import date, timedelta
+
+yesterday = date.today() - timedelta(days=1)
+startOfYesterday = yesterday.strftime("%Y-%m-%d") + " 00:00:00"
+
+binance = ccxt.binance ({ 'enableRateLimit': True })
+symbols = [ 'ETH/USDT', 'BTC/USDT', 'ADA/USDT', 'LINK/USDT', 'XRP/USDT' ]
+for symbol in symbols:
+    ohlcv_data = binance.fetch_ohlcv(symbol, '1d', binance.parse8601(startOfYesterday), 1)
+    print(ohlcv_data)
+
