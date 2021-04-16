8cc70583ec7b36e162bcd6d974471a99dcfd70ab
Diwash Rai <diwash.rai1993@gmail.com>
2021-04-16 23:10:44 +0100

    adding dot to request

--- get_prices.py
+++ get_prices.py
@@ -5,7 +5,7 @@ yesterday = date.today() - timedelta(days=1)
 startOfYesterday = yesterday.strftime("%Y-%m-%d") + " 00:00:00"
 
 binance = ccxt.binance ({ 'enableRateLimit': True })
-symbols = [ 'ETH/USDT', 'BTC/USDT', 'ADA/USDT', 'LINK/USDT', 'XRP/USDT' ]
+symbols = [ 'ETH/USDT', 'BTC/USDT', 'ADA/USDT', 'LINK/USDT', 'XRP/USDT', 'DOT/USDT' ]
 for symbol in symbols:
     ohlcv_data = binance.fetch_ohlcv(symbol, '1d', binance.parse8601(startOfYesterday), 1)
     print(ohlcv_data)
