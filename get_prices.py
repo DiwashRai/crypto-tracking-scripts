66261acc782239926ee41fe9996f9f85991883e7
Diwash Rai <diwash.rai1993@gmail.com>
2021-04-18 23:05:11 +0100

    added scripts to update crypto sheets data

--- get_prices.py
+++ get_prices.py
@@ -1,42 +1,45 @@
-# import ccxt
-# from datetime import date, timedelta
 
-# yesterday = date.today() - timedelta(days=1)
-# startOfYesterday = yesterday.strftime("%Y-%m-%d") + " 00:00:00"
-
-# binance = ccxt.binance ({ 'enableRateLimit': True })
-# symbols = [ 'ETH/USDT', 'BTC/USDT', 'ADA/USDT', 'LINK/USDT', 'XRP/USDT', 'DOT/USDT' ]
-# for symbol in symbols:
-#     ohlcv_data = binance.fetch_ohlcv(symbol, '1d', binance.parse8601(startOfYesterday), 1)
-#     print(ohlcv_data)
-
-
-from __future__ import print_function
-import os.path
+import ccxt
+import datetime
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-
 from google.oauth2 import service_account
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = 'service.json'
-
+SPREADSHEET_ID = '1PJUh6kNjcUjVTpHzBFixIM0f1EZpOGKiDOhd5h7xYeQ'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = None
 creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1PJUh6kNjcUjVTpHzBFixIM0f1EZpOGKiDOhd5h7xYeQ'
-# SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+tokens = [ 'BTC', 'ETH', 'ADA', 'LINK', 'XRP' , 'DOT']
+
+back24h = datetime.datetime.now() - datetime.timedelta(hours=24)
+back24hFormatted = back24h.strftime("%Y-%m-%d %H:%M:00")
+
+back7d = datetime.datetime.now() - datetime.timedelta(days=7)
+back7dFormatted = back7d.strftime("%Y-%m-%d %H:%M:00")
+
+update_list = []
+binance = ccxt.binance ({ 'enableRateLimit': True })
+for token in tokens:
+    print("Getting price data for ", token)
+    symbol = token + "/USDT"
+    ticker = binance.fetch_ticker(symbol)
+    tokenPriceUSDT = (float(ticker['ask']) + float(ticker['bid'])) / 2
+
+    ohlcv_data_24h = binance.fetch_ohlcv(symbol, '1m', binance.parse8601(back24hFormatted), 1)
+    change24h = (tokenPriceUSDT / ohlcv_data_24h[0][1]) - 1
+
+    ohlcv_data_7d = binance.fetch_ohlcv(symbol, '1m', binance.parse8601(back7dFormatted), 1)
+    change7d = (tokenPriceUSDT / ohlcv_data_7d[0][1]) - 1
+
+    print(token, ' current price: ', tokenPriceUSDT, ' 24h: ' , change24h, ' 7d: ', change7d)
+    update_list.append([tokenPriceUSDT, change24h, change7d])
 
 service = build('sheets', 'v4', credentials=creds)
-
-# Call the Sheets API
 sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                            range="Transactions!A1:C9").execute()
-# values = result.get('values', [])
-
-print(result)
+request = sheet.values().update(spreadsheetId=SPREADSHEET_ID, range='\'Token Prices\'!C4',
+                                valueInputOption="USER_ENTERED", body={"values":update_list}).execute()
---
+++ update_historical_closes.py
@@ -0,0 +1,89 @@
+
+import ccxt
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+
+SERVICE_ACCOUNT_FILE = 'service.json'
+SPREADSHEET_ID = '1PJUh6kNjcUjVTpHzBFixIM0f1EZpOGKiDOhd5h7xYeQ'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+tokens = [ 'BTC', 'ETH', 'ADA', 'LINK', 'XRP' , 'DOT']
+# tokens = [ 'BTC' ]
+
+
+def getThirtyDayCloses(sheet, token):
+    print("executing getThirtyDayCloses for : ", token)
+    startDay = datetime.date.today() - datetime.timedelta(days=31)
+    startDayString = startDay.strftime("%Y-%m-%d") + " 00:00:00"
+    binance = ccxt.binance ({ 'enableRateLimit': True })
+    symbol = token + "/USDT"
+    ohlcv_data = binance.fetch_ohlcv(symbol, '1d', binance.parse8601(startDayString), 30)
+
+    update_list = []
+    for x in range(30, 0, -1):
+        date = datetime.date.today() - datetime.timedelta(days=x)
+        formattedDate = date.strftime('%d/%m/%Y')
+        entry = [formattedDate, ohlcv_data[30 - x][4]]
+        update_list.append(entry)
+    
+    request = sheet.values().update(spreadsheetId=SPREADSHEET_ID, range=token+"!A3",
+                                    valueInputOption="USER_ENTERED", body={"values":update_list}).execute()
+
+
+def updateDailyCloses(sheet, token, count):
+    print("executing updateDailyCloses for : ", token)
+    lastDateCell = token + "!A" + str(count + 2)
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                            range=lastDateCell).execute()
+    lastUpdateString = result.get('values', [])[0][0]
+    nextUpdateDate = datetime.datetime.strptime(lastUpdateString, '%d-%m-%Y').date() + datetime.timedelta(days=1)
+    daysToUpdate = (datetime.date.today() - nextUpdateDate).days
+    print("Days to update for ", token, ": ", daysToUpdate)
+
+    binance = ccxt.binance ({ 'enableRateLimit': True })
+    startDayString = nextUpdateDate.strftime("%Y-%m-%d") + " 00:00:00"
+    symbol = token + "/USDT"
+    ohlcv_data = binance.fetch_ohlcv(symbol, '1d', binance.parse8601(startDayString), daysToUpdate)
+
+    update_list = []
+    for x in range(daysToUpdate):
+        formattedDate = nextUpdateDate.strftime('%d/%m/%Y')
+        nextUpdateDate = nextUpdateDate + datetime.timedelta(days=1)
+        entry = [formattedDate, ohlcv_data[x][4]]
+        update_list.append(entry)
+
+    nextDateCell = token + "!A" + str(count + 3)
+    request = sheet.values().update(spreadsheetId=SPREADSHEET_ID, range=nextDateCell,
+                                    valueInputOption="USER_ENTERED", body={"values":update_list}).execute()
+
+    print(ohlcv_data)
+
+
+def updateTokenSheet(sheet, token):
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                            range=token+"!A1").execute()
+    count = int(result.get('values', [])[0][0])
+    print(token, ' count: ', count)
+    if count < 30:
+        getThirtyDayCloses(sheet, token)
+    else:
+        updateDailyCloses(sheet, token, count)
+
+
+def main():
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+
+    for token in tokens:
+        updateTokenSheet(sheet, token)
+
+if __name__ == '__main__':
+    main()
